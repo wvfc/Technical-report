d44cd2968b472935d5028f7c71b5a9e075393231
wvfc <154296335+wvfc@users.noreply.github.com>
2025-11-29 22:51:59 -0400

    Add NetworkUtils to check internet connectivity

---
+++ app/src/main/java/com/soutech/relatoriotecnico/util/NetworkUtils.kt
@@ -0,0 +1,17 @@
+package com.soutech.relatoriotecnico.core
+
+package com.seuapp.login
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+object NetworkUtils {
+
+    fun isOnline(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = cm.activeNetwork ?: return false
+        val caps = cm.getNetworkCapabilities(network) ?: return false
+        return caps.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+}
