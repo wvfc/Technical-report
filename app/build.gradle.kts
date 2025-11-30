fd474cea343da124f1e699cfb3e095de6702db2f
wvfc <154296335+wvfc@users.noreply.github.com>
2025-11-29 22:56:41 -0400

    Add dependencies for coroutines and OkHttp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    
+
+   //loginApp
+   implementation "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3"
+   implementation "com.squareup.okhttp3:okhttp:4.12.0"
+
 }
